ad8f3ab215c22fefc23025cd9fdd9634cb20f9b8
Viet Bui <buiminhviet.2002@gmail.com>
2021-04-15 13:01:22 -0500

    Customize a few details

---
+++ Project1/style.js
@@ -0,0 +1,12 @@
+document.getElementsByClassName('top-left')[0].onclick = function(){
+    location.href= 'business-travaler.html'
+ }
+ document.getElementsByClassName('top-right')[0].onclick = function(){
+    location.href= 'dinner.html'
+ }
+ document.getElementsByClassName('bottom-left')[0].onclick = function(){
+    location.href= 'private-dining.html'
+ }
+ document.getElementsByClassName('bottom-right')[0].onclick = function(){
+    location.href= 'weekends-families.html'
+ }
