7528b202c0c166234d568e486f5f11384dcb45f2
Viet Bui <buiminhviet.2002@gmail.com>
2021-04-22 12:56:15 -0500

    Finalized the main page, now is fixing the js file

--- Project1/style.js
+++ Project1/style.js
@@ -1,12 +1,19 @@
-document.getElementsByClassName('top-left')[0].onclick = function(){
-    location.href= 'business-travaler.html'
+console.log(document.getElementsByClassName('top-left'));
+if (document.getElementsByClassName('top-left') == null) {
+   document.getElementsByClassName('top-left')[0].onclick = function(){
+      location.href= 'pages/business-traveler.html'
+   }
+}
+
+document.getElementsByClassName('top-right')[0].onclick = function(){
+   location.href= 'pages/dinner.html'
  }
- document.getElementsByClassName('top-right')[0].onclick = function(){
-    location.href= 'dinner.html'
+document.getElementsByClassName('bottom-left')[0].onclick = function(){
+   location.href= 'pages/private-dining.html'
  }
- document.getElementsByClassName('bottom-left')[0].onclick = function(){
-    location.href= 'private-dining.html'
+document.getElementsByClassName('bottom-right')[0].onclick = function(){
+   location.href= 'pages/weekends-families.html'
  }
- document.getElementsByClassName('bottom-right')[0].onclick = function(){
-    location.href= 'weekends-families.html'
- }
+document.getElementsByClassName('logo')[0].onclick = function(){
+   location.href= 'index.html'
+}
