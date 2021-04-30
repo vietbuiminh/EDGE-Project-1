f9a7fad34b57d919526c8a8a26af807067ca35e6
Viet Bui <buiminhviet.2002@gmail.com>
2021-04-29 22:27:28 -0500

    Customized the ham menu

--- Project1/style.js
+++ Project1/style.js
@@ -1,6 +1,6 @@
 var menu = document.querySelector(".menu");
 var ham = document.querySelector(".ham");
-var xIcon = document.querySelector(".xIcon");
+
 var menuIcon = document.querySelector(".menuIcon");
 
 ham.addEventListener("click", toggleMenu);
@@ -9,12 +9,10 @@ menu.classList.contains("showMenu");
 function toggleMenu() {
    if (menu.classList.contains("showMenu")) {
      menu.classList.remove("showMenu");
-     xIcon.style.display = "none";
      menuIcon.style.display = "block";
    } else {
      menu.classList.add("showMenu");
-     xIcon.style.display = "block";
-     menuIcon.style.display = "none";
+     menuIcon.style.display = "block";
    }
 }
 var menuLinks = document.querySelectorAll(".menuLink");
